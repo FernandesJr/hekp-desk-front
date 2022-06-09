221865852a718241ce50d8c44f5e8fef6e89387f
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-06-09 20:20:08 -0300

    feat: add the intercepetor

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from './../../services/auth.service';
 import { Router, Routes } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+     private authService: AuthService, 
+     private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.router.navigate(["home"]); 
+  }
+
+  logout() {
+    this.router.navigate(["login"]);
+    this.authService.logout();
+    this.toastr.info("Logout realizado com sucesso.", "Logout", {timeOut: 7000});
   }
 
 }
