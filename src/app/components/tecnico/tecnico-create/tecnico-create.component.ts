953464d87c70d43d107a1f6650fa5f48d5002f1b
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-06-14 14:20:24 -0300

    feat: push create in the /tecnicos

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
+import { Router } from '@angular/router';
+import { Tecnico } from './../../../models/Tecnico';
+import { TecnicoService } from './../../../services/tecnico.service';
 import { FormControl, Validators } from '@angular/forms'
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -14,9 +18,42 @@ export class TecnicoCreateComponent implements OnInit {
   senha = new FormControl(null, Validators.minLength(3));
   //senha2 = new UntypedFormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  tecnico: Tecnico = {
+    nome: '',
+    cpf: '',
+    email:  '',
+    senha: '',
+    perfis: []
+  }
+
+  constructor(
+    private service: TecnicoService,
+    private toastr: ToastrService, 
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  create() {
+    this.service.create(this.tecnico).subscribe(() => {
+      this.toastr.success("Usuário cadastrado com sucesso.", "Cadastro");
+      this.router.navigate(["tecnicos"]);
+    }, ex => {
+      if(ex.error.error) {
+        this.toastr.error(ex.error.message);
+      }else {
+        this.toastr.error(ex.error.errors[0].message);
+      }
+    });
+  }
+
+  addPerfil(perfil: any) {
+    if(this.tecnico.perfis.includes(perfil)){
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    }else {
+      this.tecnico.perfis.push(perfil);
+    }
   }
 
   validar(): boolean {
