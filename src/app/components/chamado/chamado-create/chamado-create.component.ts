4f3e14ac3c146613e524556480e279c3deb53236
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-06-17 14:54:31 -0300

    feat: validation form chamado-create

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chamado-create',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoCreateComponent implements OnInit {
 
+  titulo: FormControl = new FormControl(null, Validators.required);
+  status: FormControl = new FormControl(null, Validators.required);
+  prioridade: FormControl = new FormControl(null, Validators.required);
+  tecnico: FormControl = new FormControl(null, Validators.required);
+  cliente: FormControl = new FormControl(null, Validators.required);
+  descricao: FormControl = new FormControl(null, Validators.required);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampo(): boolean {
+    return this.titulo.valid && this.status.valid && 
+      this.prioridade.valid && this.tecnico.valid && 
+      this.cliente.valid && this.descricao.valid;
   }
 
 }
